db048a49a723cde0a02256241d6916487a1c7963
LeeJaeLee2610 <cuonggaayf01@gmail.com>
2022-09-27 10:34:56 +0700

    Connect Redis

--- server/src/server.js
+++ server/src/server.js
@@ -2,14 +2,16 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import mysql from "mysql2";
-
+import { createClient } from "redis";
 const PORT = 3030;
 
+// Middleware
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// connect MySQL
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -18,7 +20,10 @@ const connection = mysql.createConnection({
   database: "fasrevo",
 });
 
-console.log("OK");
+// Connect Redis
+const client = createClient();
+client.on("error", err => console.log("Redis Client Error", err));
+await client.connect();
 
 app.get("/", (req, res) => {
   connection.connect(error => {
@@ -29,6 +34,10 @@ app.get("/", (req, res) => {
     });
   });
 });
+
+await client.set("key", "value");
+const value = await client.get("key");
+console.log(value);
 
 app.listen(PORT, () => {
   console.log(`Server ${PORT}`);
