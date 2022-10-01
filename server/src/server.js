52445efd8c609dc6a15333ee1230fa764e16ebfb
LeeJaeLee2610 <cuonggaayf01@gmail.com>
2022-10-01 10:00:50 +0700

    Update Router

--- server/src/server.js
+++ server/src/server.js
@@ -3,12 +3,16 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import path from "path";
 import dotenv from "dotenv";
+import router from "./routers/index.js";
+
+const app = express();
 
 // Middleware
-const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+router(app);
 
 const __dirname = path.resolve();
 dotenv.config({ path: path.join(__dirname, "src/config/.env") });
