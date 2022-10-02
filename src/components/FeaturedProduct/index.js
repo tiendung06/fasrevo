c90c9fe82eae524cba8293132ec47ecec94f8ba8
Do Tien Dung <dungdotien14@gmail.com>
2022-10-02 08:28:31 +0700

    Commit 02/10/2022

---
+++ src/components/FeaturedProduct/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Section from '../../layout/Section';
+import List from '../List';
+
+const FeaturedProduct = () => {
+  return (
+    <Section title='Sản phẩm bán chạy'>
+      <List></List>
+    </Section>
+  );
+};
+
+export default FeaturedProduct;
--- src/layout/Section/index.js
+++ src/layout/Section/index.js
@@ -1,11 +1,17 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Section = ({ children, title }) => {
   return (
-    <section section className='w-full lg:my-10 lg:px-10 px-5 my-5'>
-      <h1 className='text-center text-xl font-bold uppercase text-black py-5 sm:py-10'>
-        {title}
-      </h1>
+    <section className='w-full lg:my-10 lg:px-[130px] px-5 my-5'>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-xl font-bold uppercase text-black py-5 sm:py-10'>
+          {title}
+        </h1>
+        <Link href='/products'>
+          <a className='text-sm hover:underline'>Xem thêm</a>
+        </Link>
+      </div>
       {children}
     </section>
   );
