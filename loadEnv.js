db32d1752a086098a83956d86a9bc61649671f86
Duy <M.Duy2001@gmail.com>
2022-12-17 22:47:44 +0700

    Update

---
+++ loadEnv.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const { argv } = require('process');
+const env = argv[2];
+
+try {
+  console.log(argv);
+  const config = fs.readFileSync(`./environment/${env}`, 'utf8');
+  fs.writeFileSync('.env.local', config, 'utf8');
+} catch (error) {
+  console.log(error);
+}
