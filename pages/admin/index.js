c41b0118cd3780b2f4924d1ce76f2bedec448546
Duy <M.Duy2001@gmail.com>
2022-12-19 01:35:24 +0700

    Update

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Main from '../../src/layout/admin/Main';
 
 const Admin = () => {
+  const { user, authenticated } = useSelector((state) => state.auth);
+
+  if (user && authenticated && user.role !== 1) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="w-10 h-10 rounded-full border-4 border-primary_red border-t-transparent border-t-4 mx-auto animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
-    <Main heading='Admin'>
+    <Main heading="Admin">
       <>
-        <h1 className='text-xl font-bold'>Trang chủ</h1>
+        <h1 className="text-xl font-bold">Trang chủ</h1>
       </>
     </Main>
   );
--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,19 @@ import Main from '../src/layout/Main';
 import FeaturedProduct from '../src/components/FeaturedProduct';
 import Category from '../src/components/Category';
 import Collections from '../src/components/Collections';
+import { useSelector } from 'react-redux';
 
 export default function Home() {
+  const { user, authenticated } = useSelector((state) => state.auth);
+
+  if (user && authenticated && user.role === 1) {
+    return (
+      <div className="h-screen w-screen flex justify-center items-center">
+        <div className="w-10 h-10 rounded-full border-4 border-primary_red border-t-transparent border-t-4 mx-auto animate-spin"></div>
+      </div>
+    );
+  }
+
   return (
     <div>
       <Main transparent={true}>
